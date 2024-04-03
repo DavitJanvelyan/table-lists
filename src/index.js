e9bfdb846ce41c94d4bf1b591b4dd0577e1daf0f
DavitJanvelyan <90098736+DavitJanvelyan@users.noreply.github.com>
2024-04-03 13:14:37 +0400

    Some changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
 import store from "./store"
-import {BrowserRouter, BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
